c48d83fc8f2acb548ab0edaa98db097cb2bbd259
aarushi3214 <76738291+aarushi3214@users.noreply.github.com>
2021-05-23 13:09:19 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,37 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display(){
+    //var title = createElement('h2')
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2, displayHeight/8);
+
+    this.input.position(displayWidth/2, displayHeight/5);
+    this.button.position(displayWidth/2+30, displayHeight/2-250);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(130, 100);
+    });
+
+  }
+}
